c666d1cb7027e9173be8f76042eed83579e9140c
juniorsysadmin <juniorsysadmin@users.noreply.github.com>
2014-08-05 15:28:20 +1000

    Whitespace changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,9 +12,11 @@ class irqbalance::install inherits irqbalance {
   # If the package is installed, but we are using a non-default init, we need
   # to kill the irqbalance if the package manager starts it by default
   # (eg. Debian)
+
   exec { 'terminate-irqbalance-instances':
     command     => '/usr/bin/pkill irqbalance',
     refreshonly => true,
     subscribe   => Package['irqbalance'],
   }
+
 }
