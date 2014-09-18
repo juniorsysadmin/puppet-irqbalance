3504f265c6074d868f8e9e8befbd9dd0858aa102
juniorsysadmin <juniorsysadmin@users.noreply.github.com>
2014-09-18 12:34:01 +1000

    Run systemctl daemon-reload when the unit service file changes

--- manifests/initscripts.pp
+++ manifests/initscripts.pp
@@ -4,6 +4,14 @@
 #
 class irqbalance::initscripts inherits irqbalance {
 
+  # If the init script file gets replaced, systemd needs to run daemon-reload.
+  # Upstart uses inotify to detect init file changes and SysV-like Inits don't
+  # care
+  $init_reload_exec = $irqbalance::service_provider ? {
+        'systemd' => 'systemd_daemon_reload',
+        default   => 'noop_init_file_change',
+      }
+
   if $irqbalance::manage_init_script_file {
 
     if $irqbalance::init_script_file_source {
@@ -14,6 +22,7 @@ class irqbalance::initscripts inherits irqbalance {
         group  => $irqbalance::init_script_file_group,
         mode   => $irqbalance::init_script_file_mode,
         owner  => $irqbalance::init_script_file_owner,
+        notify => Exec[$init_reload_exec],
       }
 
     }
@@ -26,6 +35,7 @@ class irqbalance::initscripts inherits irqbalance {
         group   => $irqbalance::init_script_file_group,
         mode    => $irqbalance::init_script_file_mode,
         owner   => $irqbalance::init_script_file_owner,
+        notify  => Exec[$init_reload_exec],
       }
 
     }
@@ -50,6 +60,16 @@ class irqbalance::initscripts inherits irqbalance {
 
     }
 
+  }
+
+  exec { 'systemd_daemon_reload':
+    command     => '/usr/bin/systemctl daemon-reload',
+    refreshonly => true,
+  }
+
+  exec { 'noop_init_file_change':
+    command     => '/bin/true',
+    refreshonly => true,
   }
 
 }
