400583651f5da00a8b50e1830be3d765b3dbf8d5
juniorsysadmin <juniorsysadmin@users.noreply.github.com>
2014-12-11 17:36:12 +1100

    Allow multiple package names

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,9 +4,8 @@ class irqbalance::install inherits irqbalance {
 
   if $irqbalance::package_manage {
 
-    package { 'irqbalance':
+    package { $irqbalance::package_name:
       ensure => $irqbalance::package_ensure,
-      name   => $irqbalance::package_name,
     }
   
   }
